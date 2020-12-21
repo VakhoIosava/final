154c270db70952d352b2c7fffc25aeff74b31d2c
vakhoiosava <vakhtangiiosava@gmail.com>
2020-12-22 03:01:37 +0400

    sixth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import ImageCarousel from './Components/ImageCarousel';
 import Posts from './Components/Posts/Post';
-import {Link, useHistory, Route, Switch} from 'react-router-dom';
-// import lg from './img/favicon.ico';
+import {Link, Route, Switch} from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import ContactPage from './Components/Contact';
@@ -10,6 +9,7 @@ import AboutPage from './Components/About';
 import MainPage from './Components/MainPage';
 import Users from './Components/Users';
 import Reviews from './Components/Reviews';
+import DarkMode from './Components/DarkMode';
 
 
 const NavBar = () => {
@@ -29,18 +29,19 @@ function App() {
     <div className="App">
       <Header></Header>
       <NavBar></NavBar>
+      <DarkMode></DarkMode>
       <Switch>
         <Route path="/" exact component={MainPage} />
         <Route path="/contact" component={ContactPage} />
         <Route path="/clients" component={Users} />
         <Route path="/reviews" component={Reviews} />
         <Route path="/about" component={AboutPage} />
-        <Route path="/projects" component={Posts} />
+        <Route path="/projects" exact component={ImageCarousel} />
+        <Route path="/projects/2" component={Posts} />
       </Switch>
       <Footer></Footer>
     </div>
   );
 }
-
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,9 +2,11 @@ import React from 'react';
 import '../App.css';
 
 const Header = () => {
-    return <div class="head" style={{textAlign: "left", padding: "0 20px"}}>
-      <h1 style={{color: "cyan", fontSize: "25px", paddingLeft: "20px"}}>Vakhtangi Iosava- Portfolio</h1>
-    </div>
-}
-  
-export default Header;
+  return (
+      <div class="head" id="heads" style={{textAlign: "left", padding: "0 20px"}}>
+        <h1 style={{color: "cyan", fontSize: "25px", paddingLeft: "20px"}}>Vakhtangi Iosava- Portfolio</h1>
+      </div>
+  )
+  }
+
+  export default Header;
