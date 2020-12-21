6ce26524f2b3524c80b124d73ed60ea4c99a169d
vakhoiosava <vakhtangiiosava@gmail.com>
2020-12-21 16:11:57 +0400

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ImageCarousel from './Components/ImageCarousel';
+import Posts from './Components/Posts/Post';
+
+function App() {
+  return (
+    <div className="App">
+    <Posts/>   
+    <ImageCarousel/>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/Components/ImgComp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../App.css'
+const ImgComp = ({ src }) => {
+    return <img src={src} alt="image" class="pic"></img>
+}
+export default ImgComp;
